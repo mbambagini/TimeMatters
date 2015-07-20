17e2f8e227da4e4ab246606afe970db32b566f2d
Mario <mario.bambagini@gmail.com>
2015-07-20 16:11:14 +0200

    Update JobEntry.java
    comment updated

--- app/src/main/java/org/timematters/database/JobEntry.java
+++ app/src/main/java/org/timematters/database/JobEntry.java
@@ -14,18 +14,38 @@ public class JobEntry implements Comparable<JobEntry> {
     private long duration = 0;
     private String descr = null;
 
+    /**
+     * Retrieve the tuple identifier
+     * 
+     * @return ID
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Set the tuple identifier
+     * 
+     * @param i new ID
+     */
     public void setId(long i) {
         id = i;
     }
 
+    /**
+     * Retrieve when the activity was saved
+     *
+     * @return the saving date
+     */
     public Date getStop() {
         return stop;
     }
 
+    /**
+     * Set the date when the activity has been done
+     * 
+     * @param s string of the date to save (compliant with the DBMS)
+     */
     public void setStop(String s) {
         if (s == null)
             stop = null;
@@ -33,18 +53,38 @@ public class JobEntry implements Comparable<JobEntry> {
             stop = Date.valueOf(s);
     }
 
+    /**
+     * Retrieve total elapsed time
+     * 
+     * @return worked interval
+     */
     public long getDuration() {
         return duration;
     }
 
+    /**
+     * Set elapsed time
+     * 
+     * @param i worked interval
+     */
     public void setDuration(long i) {
         duration = i;
     }
 
+    /**
+     * Retrieve description
+     * 
+     * @return description
+     */
     public String getDescr() {
         return descr;
     }
 
+    /**
+     * Assign a note to the current tuple
+     * 
+     * @param s description
+     */
     public void setDescr(String s) {
         if (s == null)
             descr = null;
